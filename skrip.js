a660bc1aa7600f0397ec6843337b58b3fbfa490d
Ahmad Ali Karim <98990857+EmpAhmadK@users.noreply.github.com>
2022-02-25 00:50:26 +0800

    Terbitan asas

---
+++ skrip.js
@@ -0,0 +1,37 @@
+/* JAVASCRIPT / جاۏاکسريڤ‌ت */
+
+// Modul melekat
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("melekat").classList.add("lekat");
+    document.getElementById("melekat-dalam").classList.add("lekat-sub");
+    document.getElementById("melekat-gambar").classList.add("lekat-gambar");
+    document.getElementById("melekat-kotak").classList.add("lekat-kotak");
+    document.getElementById("melekat-tajuk").classList.add("lekat-tajuk");
+    document.getElementById("subtajuk").classList.add("subtajuk-pemanjangan");
+  } else {
+    document.getElementById("melekat").classList.remove("lekat");
+    document.getElementById("melekat-dalam").classList.remove("lekat-sub");
+    document.getElementById("melekat-gambar").classList.remove("lekat-gambar");
+    document.getElementById("melekat-kotak").classList.remove("lekat-kotak");
+    document.getElementById("melekat-tajuk").classList.remove("lekat-tajuk");
+    document.getElementById("subtajuk").classList.remove("subtajuk-pemanjangan");
+  }
+}
+
+// Kemas modul tulisan
+
+function loadFunction() {
+	var iframe = document.getElementById("tulisanTerpaut");
+	var elmnta = iframe.contentWindow.document.getElementById("masthead");
+	var elmntb = iframe.contentWindow.document.getElementById("wpadminbar");
+	var elmntc = iframe.contentWindow.document.getElementsByTagName("body");
+	var elmntd = iframe.contentWindow.document.getElementsByClassName("page-header");
+	elmnta.style.display = "none";
+	elmntb.style.display = "none";
+	elmntc.style.background = "none";
+	elmntd.style.display = "none";
+}
