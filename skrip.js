ed0e0320b8a0010b73fd13b1d9772c960b453ae3
Ahmad Ali Karim <98990857+EmpAhmadK@users.noreply.github.com>
2022-02-25 11:22:22 +0800

    Header update

--- skrip.js
+++ skrip.js
@@ -6,19 +6,21 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("melekat").classList.add("lekat");
+    /* document.getElementById("melekat").classList.add("lekat");
     document.getElementById("melekat-dalam").classList.add("lekat-sub");
     document.getElementById("melekat-gambar").classList.add("lekat-gambar");
     document.getElementById("melekat-kotak").classList.add("lekat-kotak");
     document.getElementById("melekat-tajuk").classList.add("lekat-tajuk");
-    document.getElementById("subtajuk").classList.add("subtajuk-pemanjangan");
+    document.getElementById("subtajuk").classList.add("subtajuk-pemanjangan"); */
+    document.getElementById("pengepala-terapung").classList.add("apung");
   } else {
-    document.getElementById("melekat").classList.remove("lekat");
+    /* document.getElementById("melekat").classList.remove("lekat");
     document.getElementById("melekat-dalam").classList.remove("lekat-sub");
     document.getElementById("melekat-gambar").classList.remove("lekat-gambar");
     document.getElementById("melekat-kotak").classList.remove("lekat-kotak");
     document.getElementById("melekat-tajuk").classList.remove("lekat-tajuk");
-    document.getElementById("subtajuk").classList.remove("subtajuk-pemanjangan");
+    document.getElementById("subtajuk").classList.remove("subtajuk-pemanjangan"); */
+    document.getElementById("pengepala-terapung").classList.remove("apung");
   }
 }
 
